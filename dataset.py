44602239c015a384c50aaca8081292525d4f372a
Althaf Subair <althafsubair3@gmail.com>
2024-01-25 13:15:12 +0530

    Handled Outliers

--- dataset.py
+++ dataset.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from scipy import stats
+
 
 # Load dataset
 
@@ -22,6 +25,73 @@ df['previous_year_rating']=df['previous_year_rating'].fillna(df['previous_year_r
 # Droped "employee_id"
 
 df.drop('employee_id', axis=1, inplace=True)
+
+
+# Handling outliers
+
+
+# for 'age':
+
+# Calculating Quartiles
+
+Q1 = np.percentile(df['age'],25)
+Q3 = np.percentile(df['age'],75)
+
+# Calculating Inter Quartile Range
+
+IQR = Q3 - Q1
+
+# Calculating lower and upper limit
+
+low_lim = Q1 - 1.5 * IQR
+upp_lim = Q3 + 1.5 * IQR
+
+# Cliping outilers
+
+df['age'] = df['age'].clip(lower=low_lim,upper=upp_lim)
+
+
+# for 'previous_year_rating':
+
+# Calculating Quartiles
+
+Q1 = np.percentile(df['previous_year_rating'],25)
+Q3 = np.percentile(df['previous_year_rating'],75)
+
+# Calculating Inter Quartile Range
+
+IQR = Q3 - Q1
+
+# Calculating lower and upper limit
+
+low_lim = Q1 - 1.5 * IQR
+upp_lim = Q3 + 1.5 * IQR
+
+# Cliping outilers
+
+df['previous_year_rating'] = df['previous_year_rating'].clip(lower=low_lim,upper=upp_lim)
+
+
+# for 'length_of_service':
+
+# Calculating Quartiles
+
+Q1 = np.percentile(df['length_of_service'],25)
+Q3 = np.percentile(df['length_of_service'],75)
+
+# Calculating Inter Quartile Range
+
+IQR = Q3 - Q1
+
+# Calculating lower and upper limit
+
+low_lim = Q1 - 1.5 * IQR
+upp_lim = Q3 + 1.5 * IQR
+
+# Cliping outilers
+
+df['length_of_service'] = df['length_of_service'].clip(lower=low_lim,upper=upp_lim)
+
 
 
 
