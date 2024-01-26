be001dbe28dc6beecda2ef2f34ee3562d1a1bca4
Althaf Subair <althafsubair3@gmail.com>
2024-01-26 10:32:57 +0530

    Splited data into train and test

--- ml.py
+++ ml.py
@@ -8,7 +8,14 @@ from sklearn.metrics import accuracy_score, classification_report
 df = pd.read_csv('preprocessed_data.csv')
 pd.set_option('display.max_columns', None)
 
-print(df.head())
+# Assuming 'data' is your DataFrame with features and target
+X = df.drop('is_promoted', axis=1)
+y = df['is_promoted']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
 
 
 
