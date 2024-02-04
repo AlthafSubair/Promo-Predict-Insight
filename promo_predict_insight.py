6f07a84cd56af29603c930e5e75d1ae9b7ccdadf
Althaf Subair <althafsubair3@gmail.com>
2024-02-04 10:15:37 +0530

    Applied Custom oversampling

--- promo_predict_insight.py
+++ promo_predict_insight.py
@@ -2,14 +2,16 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import accuracy_score,classification_report
+from sklearn.utils import resample
+
 
 # Load dataset
 df = pd.read_csv('train_LZdllcl.csv')
 pd.set_option('display.max_columns', None)
 
-# Handling missing values
+# Handling null values
 df['education'] = df['education'].fillna(df['education'].mode()[0])
 df['previous_year_rating'] = df['previous_year_rating'].fillna(df['previous_year_rating'].median())
 
@@ -28,11 +30,14 @@ for column in columns_to_clip:
     upp_lim = Q3 + 1.5 * IQR
     df[column] = df[column].clip(lower=low_lim, upper=upp_lim)
     
-    # Encoding
+
+ # Encoding
+    
 le = LabelEncoder()
 df['department'] = le.fit_transform(df['department'])
 
-df['region'] = df['region'].str.extract('(\d+)').astype(int)
+df['region'] = df['region'].str.extract(r'(\d+)').astype(int)
+
 
 od_education = OrdinalEncoder(categories=[["Below Secondary", "Bachelor's", "Master's & above"]], dtype=int)
 df['education'] = od_education.fit_transform(df[['education']])
@@ -47,21 +52,34 @@ standard_scaler = StandardScaler()
 standard_columns = ['age', 'previous_year_rating', 'length_of_service', 'avg_training_score']
 df[standard_columns] = standard_scaler.fit_transform(df[standard_columns])
 
-# Assuming 'data' is your DataFrame with features and target
-X = df.drop('is_promoted', axis=1)
-y = df['is_promoted']
+
+
+# Separate the classes
+cls_false = df[df['is_promoted'] == 0]
+cls_true = df[df['is_promoted'] == 1]
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=119)
+X_train, X_test, y_train, y_test = train_test_split(df.drop('is_promoted', axis=1), df['is_promoted'], test_size=0.2, random_state=119)
 
-# Train model
-model_RF = RandomForestClassifier(random_state=119)
-model_RF.fit(X_train, y_train)
+# Oversample the minority class only in the training set
+oversample = resample(cls_true, n_samples=len(cls_false), random_state=119)
+X_train_oversampled = pd.concat([X_train, oversample.drop('is_promoted', axis=1)])
+y_train_oversampled = pd.concat([y_train, oversample['is_promoted']])
+
+# Initialize the Random Forest model
+model_RF = RandomForestClassifier(random_state=40)
+
+# Train the model with the oversampled training set
+model_RF.fit(X_train_oversampled, y_train_oversampled)
 
 # Make predictions on the test set
 y_pred = model_RF.predict(X_test)
 
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
-
-print(f'Accuracy: {accuracy}')
+print(f'Accuracy of : {accuracy}')
+ 
+# Print the classification report
+class_report = classification_report(y_test, y_pred)
+print(f"Classification Report of :")
+print(class_report)
